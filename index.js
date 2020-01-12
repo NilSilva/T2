feb5cb61a2e28968543e89a3bfb0deb114fd017c
Nil Silva <nil.silva.ei@gmail.com>
2020-01-12 07:21:01 +0000

    Função para fazer seed data criada.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('./config/database'); //configuração da base de dados
 const path = require('path');
 const cookie = require('cookie-parser');
+const modeloLivro = require('./app/api/models/livros');
 
 var jwt = require('jsonwebtoken');
 
@@ -36,6 +37,81 @@ app.use('/livros', validateUser, livros);
 app.get('/favicon.ico', function (req, res) {
     res.sendStatus(204);
 });
+
+//seed data
+var n;
+let listaLivros = [];
+console.log('ola');
+modeloLivro.find({}, function (err, livros) {
+    if (err) {
+        next(err);
+    } else {
+        for (let livro of livros) {
+            listaLivros.push({
+                id: livro._id,
+                nome: livro.nome,
+                dataLancamento: livro.dataLancamento
+            });
+        }
+    }
+    n = listaLivros.length;
+
+    if (n == 0) {
+        console.log('Não existem livros na base de dados.');
+        console.log('Inserir dados...');
+        seedData();
+        return;
+    }
+
+    console.log('Existem livros na base de dados.');
+    console.log('Não é necessário inserir mais.')
+});
+
+function seedData() {
+    modeloLivro.create(
+        {
+            nome: "The Lord of the Rings",
+            dataLancamento: "29/07/1954"
+        },
+        {
+            nome: "The Alchemist",
+            dataLancamento: "--/--/1988"
+        },
+        {
+            "nome": "The Little Prince",
+            "dataLancamento": "--/04/1943"
+        },
+        {
+            "nome": "grimm's Fairy Tales",
+            "dataLancamento": "1812-1858"
+        },
+        {
+            "nome": "Harry Potter and the Philosopher's Stone",
+            "dataLancamento": "26/06/1997"
+        },
+        {
+            "nome": "The Hobbit/There and Back Again",
+            "dataLancamento": "21/09/1937"
+        },
+        {
+            "nome": "And Then There Were None",
+            "dataLancamento": "06/11/1939"
+        },
+        {
+            "nome": "Dream of the Red Chamber",
+            "dataLancamento": "--/--/1868"
+        },
+        {
+            "nome": "The Lion, the Witch and the Wardrobe",
+            "dataLancamento": "16/10/1950"
+        },
+        {
+            "nome": "She: A History of Adventure",
+            "dataLancamento": "1886-1887"
+        }
+    );
+    console.log('Dados inseridos.');
+}
 
 function validateUser(req, res, next) {
     jwt.verify(req.cookies['token'], req.app.get('secretKey'), function (err, decoded) {
